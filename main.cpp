728e33d6f35febe7a1927d060bb75cd5cab277a9
dima <168024863+krovskiy@users.noreply.github.com>
2026-02-11 11:40:46 +0200

    feat: inital class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+class TypingSession {
+public:
+    void startSession(std::string text){
+        std::cout<< text <<std::endl;
+    }
+};
+
 int main(int argc, const char * argv[]){
-    std::cout<<"a";
+    TypingSession main;
+    main.startSession("test");
     return 0;
 }
