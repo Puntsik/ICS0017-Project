68ee2a750ce3b15d63f4f0efc85a87d56d0c1f60
Andreas Müller <andreasm1206@gmail.com>
2026-04-25 21:56:10 +0300

    Include utils.h and simplify charToContinue function

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,11 +1,11 @@
-#pragma once
 #include <iostream>
 #include <conio.h>
 #include <thread>
 #include <chrono>
+#include "utils.h"
 
 void charToContinue() {
 	std::cout << "\nPress any character to continue\n" << std::endl;
 	std::this_thread::sleep_for(std::chrono::seconds(2));
-	char c = _getch();
+	_getch();
 }
